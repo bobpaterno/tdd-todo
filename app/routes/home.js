1e0f8c84933c0d99f70e98d03a97741984e9681a
Robert Paterno <bobpaterno@gmail.com>
2014-06-04 12:56:57 -0500

    login works

--- app/routes/home.js
+++ app/routes/home.js
@@ -1,21 +1,33 @@
 'use strict';
 
+var traceur = require('traceur');
+var User = traceur.require(__dirname + '/../models/user.js');
+
 exports.index = (req, res)=>{
   console.log('-----Home index --------');
   res.render('home/index', {title: 'To Do Home Page'});
 };
 
 exports.login = (req, res)=>{
-console.log('-----Login  --------');
-  res.render('home/login', {});
+  console.log('in LOGIN');
+  res.render('home/login', {type:'login'});
 };
 
 exports.register = (req, res)=>{
-console.log('-----Register --------');
-  res.render('home/register', {});
+  res.render('home/login', {type:'register'});
 };
 
 exports.authenticate = (req, res)=>{
-console.log('-----Authenticate index --------');
-  res.redirect('tasks/index');
+  console.log(req.body);
+  var obj = {email: req.body.email, password: req.body.password};
+  if(req.body['auth-type'] === 'login') {
+    User.login(obj, ()=>{
+      res.redirect('tasks/index');
+    });
+  }
+  else {
+    User.register(obj, ()=>{
+      res.redirect('tasks/index');
+    });
+  }
 };
